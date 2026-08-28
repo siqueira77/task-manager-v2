64e4ffe5defce85e3b335caaaf7928829e2c56c6
Lucas Siqueira <lucasiqueira.tecnico@gmail.com>
2026-08-28 12:43:25 -0300

    refactor: atualizada  a estrutura da classe de usuario

--- src/main/java/lcs/example/taskmanager/model/User.java
+++ src/main/java/lcs/example/taskmanager/model/User.java
@@ -28,15 +28,26 @@ public class User {
         this.password = password;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
